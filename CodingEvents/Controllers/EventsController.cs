cc0f763934314defc2560d75529c504aadd354d8
Charlie <Charlie@carde-macbook.myfiosgateway.com>
2021-07-22 16:16:12 -0400

    completed chpt14 reading changes

--- CodingEvents/Controllers/EventsController.cs
+++ CodingEvents/Controllers/EventsController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CodingEvents.Data;
+using CodingEvents.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -10,14 +12,14 @@ namespace CodingEvents.Controllers
 {
     public class EventsController : Controller
     {
-         
-        static private Dictionary<string, string> Events = new Dictionary<string, string>();
+
+
         // GET: /<controller>/
         //[HttpGet]
         public IActionResult Index()
         {
-            
-            ViewBag.events = Events;
+
+            ViewBag.events = EventData.GetAll();
             return View();
         }
         [HttpGet]
@@ -26,10 +28,28 @@ namespace CodingEvents.Controllers
             return View();
         }
         [HttpPost("/Events/Add")]
-        public IActionResult NewEvent(string name, string discription)
+        public IActionResult NewEvent(Event newEvent)
         {
-            Events.Add(name, discription);
+            EventData.Add(newEvent);
             return Redirect("/Events");
+        }
+
+        public IActionResult Delete()
+        {
+            ViewBag.events = EventData.GetAll();
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Delete(int[] eventIds)
+        {
+            foreach (int eventId in eventIds)
+            {
+                EventData.Remove(eventId);
+            }
+
+        
+
+        return Redirect("/Events");
         }
     }
 }
